507f1693bc16fef7154dac241973101da6a9cfa5
bhanu <bhanu@gmail.com>
2026-09-22 13:43:36 +0530

    Add logging and gitignore

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import logging
 from data_loader import load_data
 from chatbot import get_answer
 
+logging.basicConfig(
+    filename="chatlima.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s"
+)
+
 df = load_data()
+logging.info("ChatLima started")
 
 while True:
     question = input("Ask me something (or type 'bye' to stop): ")
@@ -12,7 +20,9 @@ while True:
 
     if question.lower() == "bye":
         print("Goodbye!")
+        logging.info("ChatLima stopped by user")
         break
 
     answer = get_answer(question, df)
+    logging.info(f"Q: {question} | A: {answer}")
     print(answer)
